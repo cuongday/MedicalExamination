cd337cc04306406cc066fcaf91188ccd9021bf13
Cuong day <cuonga22k3@gmail.com>
2024-03-31 21:53:57 +0700

    disable spring security

--- src/main/java/com/MedicalExamination/Nhom3/Nhom3Application.java
+++ src/main/java/com/MedicalExamination/Nhom3/Nhom3Application.java
@@ -3,7 +3,9 @@ package com.MedicalExamination.Nhom3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class Nhom3Application {
 
 	public static void main(String[] args) {
